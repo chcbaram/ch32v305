652b978799e7d1ddc9241111c7304bd6c717ee48
Hancheol Cho <chcbaram@gmail.com>
2023-03-21 23:11:17 +0900

    UART 추가

---
+++ ch32v305_02_uart/src/bsp/device/sysmem.c
@@ -0,0 +1,30 @@
+#include <errno.h>
+#include <stdint.h>
+
+
+
+
+
+
+/*********************************************************************
+ * @fn      _sbrk
+ *
+ * @brief   Change the spatial position of data segment.
+ *
+ * @return  size: Data length
+ */
+void *_sbrk(ptrdiff_t incr)
+{
+    extern char _end[];
+    extern char _heap_end[];
+    static char *curbrk = _end;
+
+    if ((curbrk + incr < _end) || (curbrk + incr > _heap_end))
+    return NULL - 1;
+
+    curbrk += incr;
+    return curbrk - incr;
+}
+
+void _fini(){}
+void _init(){}
